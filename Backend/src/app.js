8a13f94906f5f4c62c7e9635917f6a20e093ef80
Piyush Verma <piyushvermaghga@gmail.com>
2025-12-14 14:38:44 +0530

    Fix cors configuration in app.js

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -4,9 +4,9 @@ const app = express();
 const cors = require('cors')
 const aiRoutes = require('./routes/aiRoutes');
 
-app.use(cors(
+app.use(cors({
     origin: 'https://ai-code-reviewer-frontend-8vn7.onrender.com/',
-    credentials: true
+    credentials: true, 
 ));
 app.use(express.json());
 app.use('/ai',aiRoutes);
